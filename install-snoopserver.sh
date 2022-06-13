ee3a3bfe2e6521eb2090b37fc0c77e919d8fd8b6
panda7718 <panda7718>
2022-06-13 17:32:43 +0000

    na

--- install-snoopserver.sh
+++ install-snoopserver.sh
@@ -5,4 +5,5 @@ rm -rf ~/workspace/snoop-server && \
 cd ~/workspace && \
 git clone https://github.com/panda7718/snoop-server.git && \
 cd snoop-server && \
+python3 -m pip install Django && \
 python3 manage.py runserver 0:3000 >> snoopserver.out 2>>snoopserver.out

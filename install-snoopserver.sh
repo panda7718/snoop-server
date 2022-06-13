80c13fdebd1595ac75ddbb51f67bbc72bc990ba3
panda7718 <panda7718>
2022-06-13 17:27:56 +0000

    na

--- install-snoopserver.sh
+++ install-snoopserver.sh
@@ -5,4 +5,4 @@ rm -rf ~/workspace/snoop-server && \
 cd ~/workspace && \
 git clone https://github.com/panda7718/snoop-server.git && \
 cd snoop-server && \
-python manage.py runserver 0:3000 >> snoopserver.out 2>>snoopserver.out
+python3 manage.py runserver 0:3000 >> snoopserver.out 2>>snoopserver.out

209a031ea761a260ecb87bf3cacd94402e474404
panda7718 <panda7718>
2022-06-13 17:44:01 +0000

    n

--- install-snoopserver.sh
+++ install-snoopserver.sh
@@ -1,10 +1,7 @@
-#!/usr/bin/env bash
-
-source /home/cabox/.bashrc
 mkdir -p ~/workspace/ && \
 rm -rf ~/workspace/snoop-server && \
 cd ~/workspace && \
 git clone https://github.com/panda7718/snoop-server.git && \
 cd snoop-server && \
-python3 -m pip install Django && \
+python3 -m pip install -r requirements.txt && \
 python3 manage.py runserver 0:3000 >> snoopserver.out 2>>snoopserver.out

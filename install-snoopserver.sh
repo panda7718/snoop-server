bbe8c6d095a2167549f17813b973b481292d0d9e
panda7718 <panda7718>
2022-06-16 13:17:33 +0000

    na

--- install-snoopserver.sh
+++ install-snoopserver.sh
@@ -1,7 +1,8 @@
+export PY_BIN=/home/cabox/.pyenv/shims/python3 && \
 mkdir -p ~/workspace/ && \
 rm -rf ~/workspace/snoop-server && \
 cd ~/workspace && \
 git clone https://github.com/panda7718/snoop-server.git && \
 cd snoop-server && \
-python3 -m pip install -r requirements.txt && \
-python3 manage.py runserver 0:3000 >> snoopserver.out 2>>snoopserver.out
+$PY_BIN -m pip install -r requirements.txt && \
+$PY_BIN manage.py runserver 0:3000 >> snoopserver.out 2>>snoopserver.out &
--- snoopserver/settings.py
+++ snoopserver/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'fks#gh)cdtlc(+(@ss)2s!p$5akr5zk(x&ps6r^l15zq5y96!@'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition

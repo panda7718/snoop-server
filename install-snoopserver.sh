44be5219aeba416231182fd42c026e6eaceb16dc
panda7718 <panda7718>
2022-06-29 08:11:37 +0000

    na

--- install-snoopserver.sh
+++ install-snoopserver.sh
@@ -5,4 +5,5 @@ cd ~/workspace && \
 git clone https://github.com/panda7718/snoop-server.git && \
 cd snoop-server && \
 $PY_BIN -m pip install -r requirements.txt && \
-$PY_BIN manage.py runserver 0:3000 >> snoopserver.out 2>>snoopserver.out
+export FLASK_APP=snoop-server.py && \
+flask run --host=0.0.0.0 -p 3000 >> snoopserver.out 2>>snoopserver.out
